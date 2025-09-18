5101a4ddae4c502704c1affa61272bdf40cf9c71
Shane <shane276@gmail.com>
2025-09-18 14:58:50 -0400

    Unit tests complete and functional

--- Week0.5/Task.Test/UnitTest1.cs
+++ Week0.5/Task.Test/UnitTest1.cs
@@ -6,12 +6,17 @@ using System.Net.Http.Json;
 
 namespace Web.Test;
 
-public class Response
+public class PassResponse
 {
     public bool? success { set; get; }
-    //public string? error { set; get; }
+    //public Tasc? task {set; get; }
     public string? message { set; get; }
-    //public Tasc? task { set; get; }
+}
+public class ErrResponse
+{
+    public bool? success { set; get; }
+    public string? data { set; get; }
+    public string? message { set; get; }
 }
 
 public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
@@ -26,6 +31,7 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
     // ----------- Testing all endpoints ------------
     
 
+    //Create a task
     [Fact]
     public async Task createTask()
     {
@@ -34,33 +40,95 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
         var response = await _client.PostAsJsonAsync("/api/tasks", task);
         
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var content = await response.Content.ReadFromJsonAsync<Response>();
-        content.success.Should().Be(true);
-        content.message.Should().Be("Operation completed successfully");
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
+        content?.success.Should().Be(true);
+        content?.message.Should().Be("Operation completed successfully");
     }
 
+    //get all tasks
     [Fact]
     public async Task getAll()
     {
         var response = await _client.GetAsync("/api/tasks");
         
-        
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var content = await response.Content.ReadFromJsonAsync<Response>();
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
         
-        content.success.Should().Be(true);
-        content.message.Should().Be("Operation completed successfully");
+        content?.success.Should().Be(true);
+        content?.message.Should().Be("Operation completed successfully");
     }
-    /*
+
+    //create a task and update it
+    [Fact]
+    public async Task updateTask()
     {
-    "id": 0,
-    "title": "string",
-    "description": "string",
-    "isCompleted": true,
-    "priority": 0,
-    "dueDate": "2025-09-18T14:49:56.315Z",
-    "updatedAt": "2025-09-18T14:49:56.316Z"
+        var task0 = new { title = "Make unit tests", description = "Write this code"};
+        var task = new { title = "Update this test", description = "This is a description"};
+
+        var response0 = await _client.PostAsJsonAsync("/api/tasks", task0);
+        var response = await _client.PutAsJsonAsync("/api/tasks/1", task);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
+        content?.success.Should().Be(true);
+        content?.message.Should().Be("Operation completed successfully");
     }
-    */
+    
+    //create 2 tasks and find it
+    [Fact]
+    public async Task findTask()
+    {
+        var task0 = new { title = "Make unit tests", description = "Write this code"};
+        var task1 = new { title = "Make unit tests again", description = "Write this code again"};
+
+        var response0 = await _client.PostAsJsonAsync("/api/tasks", task0);
+        var response1 = await _client.PostAsJsonAsync("/api/tasks", task1);
+        var response = await _client.GetAsync("/api/tasks/1");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
+        content?.success.Should().Be(true);
+        content?.message.Should().Be("Operation completed successfully");
+    }
+
+    //try to find a task that doesnt exist
+    [Fact]
+    public async Task ErrfindTask()
+    {
+        var response = await _client.GetAsync("/api/tasks/10");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var content = await response.Content.ReadFromJsonAsync<ErrResponse>();
+        content?.success.Should().Be(false);
+        content?.data.Should().Be("ID not found");
+        content?.message.Should().Be("Operation failed");
+    }
+
+    //create a task and delete it
+    [Fact]
+    public async Task deleteTask()
+    {
+        var task = new { title = "Make unit tests", description = "Write this code"};
+        var destroyMe = await _client.PostAsJsonAsync("/api/tasks", task);
+
+        var response = await _client.DeleteAsync("/api/tasks/1");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
+        content?.success.Should().Be(true);
+        content?.message.Should().Be("Operation completed successfully");
+    }
+
+    //try to delete a task that doesnt exist
+    [Fact]
+    public async Task ErrDeleteTask()
+    {
+        var response = await _client.DeleteAsync("/api/tasks/1");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var content = await response.Content.ReadFromJsonAsync<PassResponse>();
+        content?.success.Should().Be(false);
+        content?.message.Should().Be("Operation failed");
+    }
 }
