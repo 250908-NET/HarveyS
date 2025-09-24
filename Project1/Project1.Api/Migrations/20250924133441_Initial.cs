08f6195e1591bcbe3156913ccd0e2982b369b885
Shane <shane276@gmail.com>
2025-09-24 10:43:41 -0400

    Migration complete, ERD and README added

---
+++ Project1/Project1.Api/Migrations/20250924133441_Initial.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project1.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    starId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Moons_Stars_starId",
+                        column: x => x.starId,
+                        principalTable: "Stars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    starId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Planets_Stars_starId",
+                        column: x => x.starId,
+                        principalTable: "Stars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moons_starId",
+                table: "Moons",
+                column: "starId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planets_starId",
+                table: "Planets",
+                column: "starId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moons");
+
+            migrationBuilder.DropTable(
+                name: "Planets");
+
+            migrationBuilder.DropTable(
+                name: "Stars");
+        }
+    }
+}
---
+++ Project1/README.md
@@ -0,0 +1,11 @@
+# Project 1 (in progress)
+
+## Goal
+
+This is an application that will be able to access, record, and retrieve data about space objects, including planets, stars, and even interstellar objects
+
+## How to use
+
+You can search for any type of object, list their information, and find out which system and object they orbit.
+
+This application lets you find celestial objects within specifications by their size, length of year(if a planet), date discovered, and other statistics, to plan your vacation of course
